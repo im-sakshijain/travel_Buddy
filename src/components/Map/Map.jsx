081e4270e6ef86bac31a604aa8d8b831df4eedc8
Sakshi Jain <62291788+im-sakshijain@users.noreply.github.com>
2022-09-04 21:43:23 +0530

    changes Header

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import GoogleMapReact from 'google-map-react';
+// import { Paper,Typography, useMediaQuery } from '@material-ui/core';
+// import LocationOutlinedIcon from'@material-ui/icons/LocationOutlined';
+// import useStyles from './styles'
+
+// import Rating from '@material-ui/lab'; 
+const Map=()=>{
+   return (
+       <h1>Map</h1>
+   );
+}
+export default Map; 
